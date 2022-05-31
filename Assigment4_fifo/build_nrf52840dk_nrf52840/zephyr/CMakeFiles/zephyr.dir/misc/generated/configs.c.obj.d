8658d7af1286023220b120dadc42241dec4a2bb0
RRocha21 <renatorocha21@ua.pt>
2022-05-31 18:50:02 +0100

    remove

--- Assigment4_fifo/build_nrf52840dk_nrf52840/zephyr/CMakeFiles/zephyr.dir/misc/generated/configs.c.obj.d
+++
@@ -1,8 +0,0 @@
-zephyr/CMakeFiles/zephyr.dir/misc/generated/configs.c.obj: \
- zephyr/misc/generated/configs.c \
- C:/Users/renat/ncs/SETR/Assigment4_fifo/build_nrf52840dk_nrf52840/zephyr/include/generated/autoconf.h \
- C:/Users/renat/ncs/v1.9.1/zephyr/include/toolchain/zephyr_stdint.h \
- C:/Users/renat/ncs/v1.9.1/zephyr/include/toolchain.h \
- C:/Users/renat/ncs/v1.9.1/zephyr/include/toolchain/gcc.h \
- C:/Users/renat/ncs/v1.9.1/zephyr/include/toolchain/common.h \
- C:/Users/renat/ncs/v1.9.1/zephyr/lib/libc/minimal/include/stdbool.h
